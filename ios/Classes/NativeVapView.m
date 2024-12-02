a1a0527f4bb6487fb43efbbf25d79ae2ad07c743
Chihiro <1980705744@qq.com>
2024-12-03 06:29:24 +0800

    fix: 修复[修复多个bug]
    - 修复了iOS端多次调用同一个FlutterResult ， 与Android端是同一个错误类型

--- ios/Classes/NativeVapView.m
+++ ios/Classes/NativeVapView.m
@@ -1,118 +1,203 @@
-
 #import "NativeVapView.h"
 #import "UIView+VAP.h"
 #import "QGVAPWrapView.h"
-
+#import <Flutter/Flutter.h>
 
 @interface NativeVapView : NSObject <FlutterPlatformView, VAPWrapViewDelegate>
 
-- (instancetype)initWithFrame: (CGRect) frame
-               viewIdentifier: (int64_t) viewId
-                    arguments: (id _Nullable) args
-                    mRegistrar: (NSObject<FlutterPluginRegistrar> *) registrar;
-
-- (UIView*) view;
+- (instancetype)initWithFrame:(CGRect)frame
+               viewIdentifier:(int64_t)viewId
+                    arguments:(id _Nullable)args
+              binaryMessenger:(NSObject<FlutterBinaryMessenger> *)messenger;
 
 @end
 
-
 @implementation NativeVapViewFactory {
-    NSObject<FlutterPluginRegistrar> * _registrar;
+    NSObject<FlutterBinaryMessenger> * _messenger;
 }
 
-- (instancetype)initWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
+- (instancetype)initWithMessenger:(NSObject<FlutterBinaryMessenger> *)messenger {
     self = [super init];
     if (self) {
-        _registrar = registrar;
+        _messenger = messenger;
     }
     return self;
 }
 
-- (NSObject<FlutterPlatformView> *)createWithFrame: (CGRect) frame
-                                    viewIdentifier:(int64_t)viewId arguments:(id _Nullable)args {
-    return [[NativeVapView alloc] initWithFrame:frame viewIdentifier:viewId arguments:args mRegistrar:_registrar];
+- (NSObject<FlutterPlatformView> *)createWithFrame:(CGRect)frame
+                                    viewIdentifier:(int64_t)viewId
+                                         arguments:(id _Nullable)args {
+    return [[NativeVapView alloc] initWithFrame:frame
+                                 viewIdentifier:viewId
+                                      arguments:args
+                                binaryMessenger:_messenger];
 }
 
 @end
 
 @implementation NativeVapView {
     UIView *_view;
-    NSObject<FlutterPluginRegistrar> * _registrar;
     QGVAPWrapView *_wrapView;
-    FlutterResult _result;
-    //播放中就是ture，其他状态false
     BOOL playStatus;
+    FlutterMethodChannel *_methodChannel;
+    FlutterEventChannel *_eventChannel;
+    FlutterEventSink _eventSink;
 }
 
-- (instancetype)initWithFrame:(CGRect)frame viewIdentifier:(int64_t)viewId arguments:(id)args mRegistrar:(NSObject<FlutterPluginRegistrar> *) registrar {
-    if (self == [super init]) {
-        playStatus = false;
-        _view = [[UIView alloc] init];
-        _registrar = registrar;
-        FlutterMethodChannel* channel = [FlutterMethodChannel
-            methodChannelWithName:@"flutter_vap_controller"
-                  binaryMessenger:registrar.messenger];
-        
-        [registrar addMethodCallDelegate: self channel:channel];
-        
+- (instancetype)initWithFrame:(CGRect)frame
+               viewIdentifier:(int64_t)viewId
+                    arguments:(id _Nullable)args
+              binaryMessenger:(NSObject<FlutterBinaryMessenger> *)messenger {
+    self = [super init];
+    if (self) {
+        playStatus = NO;
+        _view = [[UIView alloc] initWithFrame:frame];
+
+        // Initialize MethodChannel
+        NSString *methodChannelName = [NSString stringWithFormat:@"flutter_vap_controller_%lld", viewId];
+        _methodChannel = [FlutterMethodChannel methodChannelWithName:methodChannelName binaryMessenger:messenger];
+        [_methodChannel setMethodCallHandler:^(FlutterMethodCall *call, FlutterResult result) {
+            [self handleMethodCall:call result:result];
+        }];
+
+        // Initialize EventChannel
+        NSString *eventChannelName = [NSString stringWithFormat:@"flutter_vap_event_channel_%lld", viewId];
+        _eventChannel = [FlutterEventChannel eventChannelWithName:eventChannelName binaryMessenger:messenger];
+        __weak typeof(self) weakSelf = self;
+        [_eventChannel setStreamHandler:self];
     }
     return self;
 }
 
-#pragma mark --flutter调native回调
+#pragma mark - FlutterPlatformView
+
+- (UIView *)view {
+    return _view;
+}
+
+#pragma mark - FlutterStreamHandler
+
+- (FlutterError * _Nullable)onListenWithArguments:(id _Nullable)arguments eventSink:(FlutterEventSink)events {
+    _eventSink = events;
+    return nil;
+}
+
+- (FlutterError * _Nullable)onCancelWithArguments:(id _Nullable)arguments {
+    _eventSink = nil;
+    return nil;
+}
+
+#pragma mark - Method Call Handling
+
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-    _result = result;
-    if ([@"playPath" isEqualToString: call.method]) {
-        [self playByPath:call.arguments[@"path"]];
-    } else if ([@"playAsset" isEqualToString:call.method]) {
-        //播放asset文件
-        NSString* assetPath = [_registrar lookupKeyForAsset:call.arguments[@"asset"]];
-        NSString* path = [[NSBundle mainBundle] pathForResource:assetPath ofType:nil];
-        [self playByPath:path];
-    } else if ([@"stop" isEqualToString:call.method]) {
-        if (_wrapView) {
-            [_wrapView removeFromSuperview];
+    if ([@"playPath" isEqualToString:call.method]) {
+        NSString *path = call.arguments[@"path"];
+        if (path) {
+            [self playByPath:path withResult:result];
+        } else {
+            result([FlutterError errorWithCode:@"INVALID_ARGUMENT"
+                                       message:@"Path is null"
+                                       details:nil]);
         }
-        playStatus = false;
+    } else if ([@"playAsset" isEqualToString:call.method]) {
+        NSString *asset = call.arguments[@"asset"];
+        if (asset) {
+            NSString *assetPath = [[NSBundle mainBundle] pathForResource:asset ofType:nil];
+            if (assetPath) {
+                [self playByPath:assetPath withResult:result];
+            } else {
+                result([FlutterError errorWithCode:@"ASSET_NOT_FOUND"
+                                           message:@"Asset not found"
+                                           details:nil]);
+            }
+        } else {
+            result([FlutterError errorWithCode:@"INVALID_ARGUMENT"
+                                       message:@"Asset is null"
+                                       details:nil]);
+        }
+    } else if ([@"stop" isEqualToString:call.method]) {
+        [self stopPlayback];
+        result(nil);
+    } else {
+        result(FlutterMethodNotImplemented);
     }
 }
 
-- (void)playByPath:(NSString *)path{
-    //限制只能有一个视频在播放
+#pragma mark - Playback Control
+
+- (void)playByPath:(NSString *)path withResult:(FlutterResult)result {
     if (playStatus) {
+        result([FlutterError errorWithCode:@"ALREADY_PLAYING"
+                                   message:@"A video is already playing"
+                                   details:nil]);
         return;
     }
-    _wrapView = [[QGVAPWrapView alloc] initWithFrame:self.view.bounds];
-    _wrapView.center = self.view.center;
+
+    playStatus = YES;
+    _wrapView = [[QGVAPWrapView alloc] initWithFrame:_view.bounds];
+    _wrapView.center = _view.center;
     _wrapView.contentMode = QGVAPWrapViewContentModeAspectFit;
-    _wrapView.autoDestoryAfterFinish = YES;
-    [self.view addSubview:_wrapView];
+    _wrapView.autoDestroyAfterFinish = YES;
+    _wrapView.delegate = self;
+    [_view addSubview:_wrapView];
     [_wrapView vapWrapView_playHWDMP4:path repeatCount:0 delegate:self];
+
+    // Optionally, you can notify Flutter that playback has started
+    if (_eventSink) {
+        _eventSink(@{@"status": @"started"});
+    }
 }
 
+- (void)stopPlayback {
+    if (_wrapView) {
+        [_wrapView removeFromSuperview];
+        _wrapView = nil;
+    }
+    playStatus = NO;
 
-#pragma mark VAPWrapViewDelegate--播放回调
-- (void) vapWrap_viewDidStartPlayMP4:(VAPView *)container{
-    playStatus = true;
+    // Notify Flutter that playback has stopped
+    if (_eventSink) {
+        _eventSink(@{@"status": @"stopped"});
+    }
 }
 
-- (void) vapWrap_viewDidFailPlayMP4:(NSError *)error{
-    NSDictionary *resultDic = @{@"status":@"failure",@"errorMsg":error.description};
-    _result(resultDic);
+#pragma mark - VAPWrapViewDelegate
+
+- (void)vapWrap_viewDidStartPlayMP4:(VAPView *)container {
+    playStatus = YES;
+
+    // Notify Flutter that playback has started
+    if (_eventSink) {
+        _eventSink(@{@"status": @"started"});
+    }
 }
 
-- (void) vapWrap_viewDidStopPlayMP4:(NSInteger)lastFrameIndex view:(VAPView *)container{
-    playStatus = false;
-}
-    
--(void) vapWrap_viewDidFinishPlayMP4:(NSInteger)totalFrameCount view:(VAPView *)container {
-    NSDictionary *resultDic = @{@"status":@"complete"};
-    _result(resultDic);
-    playStatus = false;
+- (void)vapWrap_viewDidFailPlayMP4:(NSError *)error {
+    playStatus = NO;
+
+    if (_eventSink) {
+        _eventSink(@{
+                           @"status": @"failure",
+                           @"errorMsg": error.localizedDescription ?: @"Unknown error"
+                   });
+    }
 }
 
-- (UIView*)view {
-    return _view;
+- (void)vapWrap_viewDidStopPlayMP4:(NSInteger)lastFrameIndex view:(VAPView *)container {
+    playStatus = NO;
+
+    // Notify Flutter that playback has stopped
+    if (_eventSink) {
+        _eventSink(@{@"status": @"stopped"});
+    }
+}
+
+- (void)vapWrap_viewDidFinishPlayMP4:(NSInteger)totalFrameCount view:(VAPView *)container {
+    playStatus = NO;
+
+    if (_eventSink) {
+        _eventSink(@{@"status": @"complete"});
+    }
 }
 
 @end
