2ae8ee3370f65f348f73cd35e924a3d98a762428
Chihiro <1980705744@qq.com>
2024-12-02 23:54:17 +0800

    fix: 修复[修复多个bug]
    - 修复了Flutter 3.2以上版本适配问题
    - 修复了Android 端java.lang.IllegalStateException: Reply already submitted异常，这个异常是由于多次调用MethodChannel的success和error导致

--- lib/vap_controller.dart
+++ lib/vap_controller.dart
@@ -2,20 +2,34 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class VapController {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_vap_controller');
+  static const MethodChannel _methodChannel = MethodChannel('flutter_vap_controller');
+  static const EventChannel _eventChannel = EventChannel('flutter_vap_event_channel');
+
+  void init() {
+    _eventChannel.receiveBroadcastStream().listen(_onEvent, onError: _onError);
+  }
+
+  void _onEvent(dynamic event) {
+    // Handle the event
+    print('Received event: $event');
+  }
+
+  void _onError(Object error) {
+    // Handle the error
+    print('Error receiving event: $error');
+  }
 
   /// return: play error:       {"status": "failure", "errorMsg": ""}
   ///         play complete:    {"status": "complete"}
   static Future<Map<dynamic, dynamic>?> playPath(String path) async {
-    return _channel.invokeMethod('playPath', {"path": path});
+    return _methodChannel.invokeMethod('playPath', {"path": path});
   }
 
   static Future<Map<dynamic, dynamic>?> playAsset(String asset) {
-    return _channel.invokeMethod('playAsset', {"asset": asset});
+    return _methodChannel.invokeMethod('playAsset', {"asset": asset});
   }
 
   static stop() {
-    _channel.invokeMethod('stop');
+    _methodChannel.invokeMethod('stop');
   }
 }
